7ed903987290197394f23049af224dd640ea3569
Romain Durand <duran2101@gmail.com>
2026-04-07 16:41:40 -0400

    move layer toggle to RWIN to get back RALT

--- keyboards/keychron/k5_max/ansi/white/keymaps/colemak_dh/keymap.c
+++ keyboards/keychron/k5_max/ansi/white/keymaps/colemak_dh/keymap.c
@@ -67,8 +67,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         FR_HASH,  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,  KC_INS,   KC_HOME,  KC_PGUP,  KC_NUM,   KC_PSLS,  KC_PAST,  KC_PMNS,
         KC_TAB,   KC_Q,     KC_W,     KC_F,     KC_P,     KC_B,     KC_J,     KC_L,     KC_U,     KC_Y,     KC_COLN,  FR_LBRC,  FR_RBRC,  FR_BSLS,  KC_DEL,   KC_END,   KC_PGDN,  KC_P7,    KC_P8,    KC_P9,
         KC_CAPS,  KC_A,     KC_R,     KC_S,     KC_T,     KC_G,     KC_M,     KC_N,     KC_E,     KC_I,     KC_O,     FR_QUOT,            KC_ENT,                                 KC_P4,    KC_P5,    KC_P6,    KC_PPLS,
-        KC_LSFT,            KC_X,     KC_C,     KC_D,     KC_V,     KC_Z,     KC_K,     KC_H,     KC_COMM,  KC_DOT,   FR_SLSH,            KC_RSFT,            KC_UP,    KC_P1,    KC_P2,    KC_P3,
-        KC_LCTL,  KC_LWIN,  KC_LALT,                                KC_SPC,                                 TT(_COLEMAK_FR),KC_RWIN,MO(_COLEMAK_FN),KC_RCTL,KC_LEFT,KC_DOWN,KC_RGHT,KC_P0,            KC_PDOT,  KC_PENT
+        KC_LSFT,            KC_X,     KC_C,     KC_D,     KC_V,     KC_Z,     KC_K,     KC_H,     KC_COMM,  KC_DOT,   FR_SLSH,            KC_RSFT,            KC_UP,              KC_P1,    KC_P2,    KC_P3,
+        KC_LCTL,  KC_LWIN,  KC_LALT,                                KC_SPC,                                 KC_RALT,TT(_COLEMAK_FR),MO(_COLEMAK_FN),KC_RCTL,KC_LEFT,KC_DOWN,KC_RGHT,KC_P0,            KC_PDOT,  KC_PENT
     ),
 
 /* LAYER 1: Colemak FN - F-keys, media
@@ -118,7 +118,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,   KC_Q,     KC_W,     KC_F,     KC_P,     KC_B,     KC_J,     KC_L,     KC_U,     KC_Y,     KC_COLN,  FR_DCIR,  FR_CEDL,  FR_DGRV, KC_DEL,   KC_END,   KC_PGDN,  KC_P7,    KC_P8,    KC_P9,
         KC_CAPS,  KC_A,     KC_R,     KC_S,     KC_T,     KC_G,     KC_M,     KC_N,     KC_E,     KC_I,     KC_O,     FR_QUOT,            KC_ENT,                                KC_P4,    KC_P5,    KC_P6,    KC_PPLS,
         KC_LSFT,            KC_X,     KC_C,     KC_D,     KC_V,     KC_Z,     KC_K,     KC_H,     KC_COMM,  KC_DOT,   FR_EACU,            KC_RSFT,           KC_UP,              KC_P1,    KC_P2,    KC_P3,
-        KC_LCTL,  KC_LWIN,  KC_LALT,                                KC_SPC,                                 _______,  KC_RWIN,MO(_COLEMAK_FN),KC_RCTL,KC_LEFT,KC_DOWN, KC_RGHT,  KC_P0,              KC_PDOT,  KC_PENT
+        KC_LCTL,  KC_LWIN,  KC_LALT,                                KC_SPC,                                 KC_RALT,  _______,MO(_COLEMAK_FN),KC_RCTL,KC_LEFT,KC_DOWN, KC_RGHT,  KC_P0,              KC_PDOT,  KC_PENT
     ),
 
 };
