869fe15b97fca610d607b3894db29922e1e05475
Romain Durand <duran2101@gmail.com>
2026-04-21 13:27:27 -0400

    tap dance update

--- keyboards/keychron/k5_max/ansi/white/keymaps/colemak_dh/README.md
+++ keyboards/keychron/k5_max/ansi/white/keymaps/colemak_dh/README.md
@@ -8,15 +8,25 @@ I've tried for a few weeks with the following configuration:
 - Colemak-DH with Angle mod on "Mac" layer (physical switch at the back of the keyboard)
 - Colemak-DH with Angle mod on 5th layer, accessible upon double tap on RWin
 
-This last layer differed from the other Colemak one (layer 0) by a few French Canadian specific aliases, notably for "é" and a few dead keys.
+This last layer differed from the other Colemak one (layer 0) by a few French Canadian specific aliases, notably for `é` and a few dead keys.
 
 My rationale was that I needed to have an English/coding oriented layer, while the French layer would be separate for punctual use.
 
-In the end, I find almost all keys too far to reach, especially ` (above Enter) and decided to move to the use of a leader key (CAPS).
+In the end, I find almost all keys too far to reach, especially `` ` `` (above `Enter`) and decided to use a leader key (`CAPS`) to define combos.
 
-Ultimately I should be able to get rid of that last layer which isn't really useful.
+## Latest improvements
 
-Another thing that I want to do is to implement some custom combos like `` s + t = ` ``
+Leader combos:
+- QK_LEAD (`CAPS`), `a` = `à`
+- QK_LEAD (`CAPS`), `u` = `ù`
+- QK_LEAD (`CAPS`), `c` = `ç`
+
+I'm now typing `é` by double tapping `e` (tap dance). It's intuitive, practical (given its frequency in French) and so far I love it. Unfortunately it's kinda difficult to combine with a leader combo.
+In order to remedy this, I'm temporarily using `n` to type `è` and `ê`:
+- QK_LEAD (`CAPS`), `n` = `è`
+- QK_LEAD (`CAPS`), `f`, `n` = `ê`
+
+Leader combos are being tested and are likely to change.
 
 ## Visual
 
@@ -25,6 +35,12 @@ Using [keymap-drawer](https://github.com/caksoylar/keymap-drawer/tree/main), I g
 Then, I edited manually and generated a SVG with `keymap draw keymap.yaml -j ../../../../info.json >keymap.svg`
 
 > [!WARNING]
-> The visual **does not** currently include the newly implemented leader key combos
+> The visual **only** includes **basic** stuff and therefore doesn't currently display shifts, leader combos or tap dance actions. 
 
 ![Keymap layout](./keymap.svg)
+
+## Roadmap
+
+- [ ] 3-key leader combos should actually be: leader, deadkey
+- [ ] Deal with `` ` `` (chording would require two letters that are never adjacent in French or English... maybe not the best)
+- [ ] Pool layers `COLEMAK` and `COLEMAK_FR`
--- keyboards/keychron/k5_max/ansi/white/keymaps/colemak_dh/keymap.c
+++ keyboards/keychron/k5_max/ansi/white/keymaps/colemak_dh/keymap.c
@@ -48,6 +48,17 @@ const key_override_t *key_overrides[] = {
     &s_frbslash_override
 };
 
+// Tap dance declarations
+enum {
+    TD_EACU,
+};
+
+// Tap Dance definitions
+tap_dance_action_t tap_dance_actions[] = {
+    // Tap once for Escape, twice for Caps Lock
+    [TD_EACU] = ACTION_TAP_DANCE_DOUBLE(KC_E, FR_EACU),
+};
+
 // set leader key combos
 void leader_start_user(void) {
     // Do something when the leader key is pressed
@@ -55,13 +66,8 @@ void leader_start_user(void) {
 
 void leader_end_user(void) {
     // --- TWO KEY SEQUENCES FIRST ---
-    if (leader_sequence_two_keys(KC_E, KC_E)) {
-        // Leader + e + e = è
-        tap_code16(FR_DGRV);
-        tap_code16(KC_E);
-
-    } else if (leader_sequence_two_keys(KC_F, KC_E)) {
-        // Leader + f + e = ê
+    if (leader_sequence_two_keys(KC_F, KC_N)) {
+        // Leader + f + n = ê
         tap_code16(FR_DCIR);
         tap_code16(KC_E);
 
@@ -70,15 +76,26 @@ void leader_end_user(void) {
         tap_code16(FR_DCIR);
         tap_code16(KC_A);
 
+    } else if (leader_sequence_two_keys(KC_F, KC_O)) {
+        // Leader + f + o = ô
+        tap_code16(FR_DCIR);
+        tap_code16(KC_O);
+
     } else if (leader_sequence_two_keys(KC_F, KC_U)) {
         // Leader + f + u = û
         tap_code16(FR_DCIR);
         tap_code16(KC_U);
 
+    } else if (leader_sequence_two_keys(KC_F, KC_I)) {
+        // Leader + f + i = î
+        tap_code16(FR_DCIR);
+        tap_code16(KC_I);
+
     // --- ONE KEY SEQUENCES LAST ---
-    } else if (leader_sequence_one_key(KC_E)) {
-        // Leader + e = é
-        tap_code16(FR_EACU);
+    } else if (leader_sequence_one_key(KC_N)) {
+        // Leader + n = è
+        tap_code16(FR_DGRV);
+        tap_code16(KC_E);
 
     } else if (leader_sequence_one_key(KC_A)) {
         // Leader + a = à
@@ -165,8 +182,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,             KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,  KC_PSCR,  KC_CTANA, BL_STEP,  _______,  _______,  _______,  _______,
         FR_HASH,  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC, KC_INS,   KC_HOME,  KC_PGUP,  KC_NUM,   KC_PSLS,  KC_PAST,  KC_PMNS,
         KC_TAB,   KC_Q,     KC_W,     KC_F,     KC_P,     KC_B,     KC_J,     KC_L,     KC_U,     KC_Y,     KC_COLN,  FR_DCIR,  FR_CEDL,  FR_DGRV, KC_DEL,   KC_END,   KC_PGDN,  KC_P7,    KC_P8,    KC_P9,
-        QK_LEAD,  KC_A,     KC_R,     KC_S,     KC_T,     KC_G,     KC_M,     KC_N,     KC_E,     KC_I,     KC_O,     FR_QUOT,            KC_ENT,                                KC_P4,    KC_P5,    KC_P6,    KC_PPLS,
-        KC_LSFT,            KC_X,     KC_C,     KC_D,     KC_V,     KC_Z,     KC_K,     KC_H,     KC_COMM,  KC_DOT,   FR_EACU,            KC_RSFT,           KC_UP,              KC_P1,    KC_P2,    KC_P3,
+        QK_LEAD,  KC_A,     KC_R,     KC_S,     KC_T,     KC_G,     KC_M,     KC_N,     TD(TD_EACU),KC_I,   KC_O,     FR_QUOT,            KC_ENT,                                KC_P4,    KC_P5,    KC_P6,    KC_PPLS,
+        KC_LSFT,            KC_X,     KC_C,     KC_D,     KC_V,     KC_Z,     KC_K,     KC_H,     KC_COMM,  KC_DOT,   FR_SLSH,            KC_RSFT,           KC_UP,              KC_P1,    KC_P2,    KC_P3,
         KC_LCTL,  KC_LWIN,  KC_LALT,                                KC_SPC,                                 KC_RALT,  _______,MO(_COLEMAK_FN),KC_RCTL,KC_LEFT,KC_DOWN, KC_RGHT,  KC_P0,              KC_PDOT,  KC_PENT
     ),
 
